3fe7548c87af5e8cbb933f53d62412e8b5a4c759
mofopeadegoke <mofopeadegoke@gmail.com>
2024-03-11 09:36:34 +0200

    Features section made

--- react-app/src/pages/home.jsx
+++ react-app/src/pages/home.jsx
@@ -4,6 +4,8 @@ import Button from "../components/button";
 import stickyNote from "../images/note-sticky.svg";
 import pencil from "../images/pencil.svg";
 import download from "../images/download.svg";
+import infinity from "../images/infinity.svg";
+import check from "../images/check.svg"
 
 export default function Home() {
     return (
@@ -35,8 +37,47 @@ export default function Home() {
                     </article>
                     <article className="feature">
                         <img src={download} alt="A download icon" />
-                        <p className="feahead">Save notes locally</p>
+                        <p className="feahead" role="heading" aria-roledescription="heading">Save notes locally</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor doloribus nobis repellendus quasi rem, vero excepturi illum ea voluptate quam.</p>
+                    </article>
+                </article>
+            </article>
+            <article className="plans">
+                <h2>Inscribe Plans</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi illo fugiat qui nisi aliquam sit dolor nihil laborum exercitationem distinctio?</p>
+                <article className="plan-details">
+                    <article className="freePlan">
+                        <article className="priceSection">
+                            <p role="heading" aria-roledescription="heading">Inscribe Free</p>
+                            <p><span className="lg">Free</span></p>
+                            <p><span className="highlight">10</span> Local Saves/mo</p>
+                            <p className="row"><img src={infinity} alt="The infinity symbol" /> Online Notes</p>
+                            <Button className="inv-sc-btn sc-btn" value="Select"/>
+                        </article>
+                        <article className="whatsIncluded">
+                            <p role="heading" aria-roledescription="heading">WHAT'S INCLUDED: </p>
+                            <ul>
+                                <li> <img src={check} alt="A green check symbol" />Sticky Notes on any Website</li>
+                                <li> <img src={check} alt="A green check symbol" />Speech to Text Features</li>
+                                <li> <img src={check} alt="A green check symbol" />Rich Text Features</li>
+                            </ul>
+                        </article>
+                    </article>
+                    <article className="premiumPlan">
+                        <article className="priceSection">
+                            <p role="heading" aria-roledescription="heading">Inscribe Premium</p>
+                            <p><span className="lg">$2.99</span>/mo</p>
+                            <p className="row"><img src={infinity} alt="The infinity symbol" /> Local Saves</p>
+                            <p className="row"><img src={infinity} alt="The infinity symbol" /> Online Notes</p>
+                            <Button className="inv-sc-btn sc-btn" value="Select"/>
+                        </article>
+                        <article className="whatsIncluded">
+                            <p role="heading" aria-roledescription="heading">ALL FREE PLUS: </p>
+                            <ul>
+                                <li> <img src={check} alt="A green check symbol" />Drawing Features</li>
+                                <li> <img src={check} alt="A green check symbol" />Save Drawings Locally</li>
+                            </ul>
+                        </article>
                     </article>
                 </article>
             </article>
