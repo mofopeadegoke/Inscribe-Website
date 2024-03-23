c4a134fdd9d1399c1a8ac72907764e82f139173f
mofopeadegoke <mofopeadegoke@gmail.com>
2024-03-23 12:02:09 +0200

    Web and Mobile view almost done

--- react-app/src/components/nav.jsx
+++ react-app/src/components/nav.jsx
@@ -1,20 +1,68 @@
 import Logo from "../images/logoNoBgNPad.png";
-import "../styles/nav.css"
+import "../styles/nav.css";
+import "../styles/responsive.css";
 import Button from "./button";
+import bars from "../images/bars.svg";
+import { useEffect } from "react";
 export default function Navbar() {
+    useEffect(() => {
+        const desktopEls = document.querySelectorAll(".onDesktop");
+        const mobileEls = document.querySelectorAll(".onMobile");
+        const barEl = document.querySelector(".bar");
+        const mobileNavEl = document.querySelector("div.links");
+        let isMobNav = false;
+        if (window.innerWidth <= 1000) {
+                desktopEls.forEach(el => {
+                    el.style.display = "none";
+                });
+                mobileEls.forEach(el => {
+                    el.style.display = "block";
+                })
+        }
+        window.addEventListener('resize', () => {
+            if (window.innerWidth <= 1000) {
+                desktopEls.forEach(el => {
+                    el.style.display = "none";
+                });
+                mobileEls.forEach(el => {
+                    el.style.display = "block";
+                })
+            } else {
+               desktopEls.forEach(el => {
+                    el.style.display = "block";
+                });
+                mobileEls.forEach(el => {
+                    el.style.display = "none";
+                }) 
+            }
+        })
+        barEl.addEventListener("click", (e) => {
+            e.stopImmediatePropagation()
+            mobileNavEl.classList.toggle('show');
+        })
+    }, [])
     return (
         <>
             <nav className="navbar">
                 <ul className="left-side">
                     <li className="imgLi"><img src={Logo} alt="The color-inverted form of the Inscribe Logo" /> <span>Inscribe</span></li>
-                    <li>Home</li>
-                    <li>Plans</li>
+                    <li className="onDesktop">Home</li>
+                    <li className="onDesktop">Plans</li>
                 </ul>
                 <ul className="right-side">
-                    <li>Help</li>
-                    <li><Button className="sc-btn inv-sc-btn" value="Get Started"/></li>
+                    <li className="onDesktop">Help</li>
+                    <li className="onDesktop"><Button className="sc-btn inv-sc-btn" value="Get Started"/></li>
+                    <img src={bars} alt="Three black horizontal bars" className="onMobile bar"/>
                 </ul>
             </nav>
+            <div className="links">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Plans</a></li>
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#">Get Started</a></li>
+                </ul>
+            </div>
         </>
     )
 }
--- react-app/src/pages/home.jsx
+++ react-app/src/pages/home.jsx
@@ -1,5 +1,7 @@
 import Navbar from "../components/nav"
 import "../styles/home.css";
+import "../styles/responsive.css";
+import { useEffect } from "react";
 import Button from "../components/button";
 import stickyNote from "../images/note-sticky.svg";
 import pencil from "../images/pencil.svg";
@@ -12,17 +14,25 @@ import Logo from "../images/logoNoBgNPad.png";
 import facebook from "../images/facebookGray.svg";
 import instagram from "../images/instagramGray.svg";
 import xTwitter from "../images/xGray.svg";
+// Importing Animate on Scroll
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function Home() {
+    useEffect(() => {
+        Aos.init({duration: 1000});
+    }, [])
     return (
         <>
             <Navbar />
             <article className="introSection">
                 <article className="content">
-                    <h1>The form solution for any developer</h1>
-                    <p>Use your own frontend code. Submit to our API. We'll handle the rest.</p>
-                    <Button className="sc-btn inv-sc-btn" value="Get Started"/>
-                    <article className="psSection">
+                    <h1 data-aos="fade-down">The form solution for any developer</h1>
+                    <p data-aos="fade-up">Use your own frontend code. Submit to our API. We'll handle the rest.</p>
+                    <article data-aos="fade-left">
+                        <Button className="sc-btn inv-sc-btn" value="Get Started"/>
+                    </article>
+                    <article className="psSection" data-aos="fade-up">
                         <b>Formspree forms can perfectly match your website</b>
                         <p>by inheriting your website's CSS</p>
                     </article>
@@ -31,28 +41,28 @@ export default function Home() {
             <article className="features">
                 <h2>Features</h2>
                 <article className="featuresContainer">
-                    <article className="feature">
+                    <article className="feature" data-aos={(window.innerWidth <= 1000) ? "fade-right" : "fade-up"}>
                         <img src={stickyNote} alt="A sticky note icon" />
                         <p className="feahead">Sticky Notes</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor doloribus nobis repellendus quasi rem, vero excepturi illum ea voluptate quam.</p>
                     </article>
-                    <article className="feature">
+                    <article className="feature" data-aos={(window.innerWidth <= 1000) ? "fade-left" : "fade-up"}>
                         <img src={pencil} alt="A pencil icon" />
                         <p className="feahead">Drawing features</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor doloribus nobis repellendus quasi rem, vero excepturi illum ea voluptate quam.</p>
                     </article>
-                    <article className="feature">
+                    <article className="feature" data-aos={(window.innerWidth <= 1000) ? "fade-right" : "fade-up"}>
                         <img src={download} alt="A download icon" />
                         <p className="feahead" role="heading" aria-roledescription="heading">Save notes locally</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor doloribus nobis repellendus quasi rem, vero excepturi illum ea voluptate quam.</p>
                     </article>
                 </article>
             </article>
-            <article className="plans">
+            <article className="plans" data-aos="fade-up">
                 <h2>Inscribe Plans</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi illo fugiat qui nisi aliquam sit dolor nihil laborum exercitationem distinctio?</p>
                 <article className="plan-details">
-                    <article className="freePlan">
+                    <article className="freePlan" data-aos={(window.innerWidth <= 1000) ? "fade-up" : "fade-right"}>
                         <article className="priceSection">
                             <p role="heading" aria-roledescription="heading">Inscribe Free</p>
                             <p><span className="lg">Free</span></p>
@@ -69,7 +79,7 @@ export default function Home() {
                             </ul>
                         </article>
                     </article>
-                    <article className="premiumPlan">
+                    <article className="premiumPlan" data-aos={(window.innerWidth <= 1000) ? "fade-up" : "fade-left"}>
                         <article className="priceSection">
                             <p role="heading" aria-roledescription="heading">Inscribe Premium</p>
                             <p><span className="lg">$2.99</span>/mo</p>
@@ -87,14 +97,14 @@ export default function Home() {
                     </article>
                 </article>
             </article>
-            <article className="trustedSection">
+            <article className="trustedSection" data-aos="flip-left">
                 <article className="container">
                     <p>Trusted by 1500+ users across Chrome and Edge with 4000+ installs.</p>
                 </article>
             </article>
             <article className="howItWorks">
-                <img src={completed} alt="An illustraions of a person chechong boxes" />
-                <article className="content">
+                <img src={completed} alt="An illustraions of a person chechong boxes" data-aos="fade-right"/>
+                <article className="content" data-aos="fade-left">
                     <h2>How it works</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae corrupti voluptatum unde! Debitis, voluptatum sed quod sequi recusandae rerum quia explicabo dolore nulla obcaecati reprehenderit non velit, suscipit doloremque incidunt.</p>
                     <ul>
@@ -106,7 +116,7 @@ export default function Home() {
                     <p>For more information <a href="#">Check out our Instagram page</a></p>
                 </article>
             </article>
-            <article className="helpSection">
+            <article className="helpSection" data-aos="fade-up">
                 <article className="socialLinks">
                     <img src={Logo} alt="Inscribe Logo" />
                     <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, tempora!</h2>
