9699e51cf6a72d8621b08f821fdd3c073f3e5a83
mofopeadegoke <mofopeadegoke@gmail.com>
2024-04-20 13:19:01 +0300

    corrections made

--- react-app/src/components/nav.jsx
+++ react-app/src/components/nav.jsx
@@ -5,83 +5,97 @@ import Button from "./button";
 import bars from "../images/bars.svg";
 import { useEffect } from "react";
 export default function Navbar(props) {
-    useEffect(() => {
-        const desktopEls = document.querySelectorAll(".onDesktop");
-        const mobileEls = document.querySelectorAll(".onMobile");
-        const barEl = document.querySelector(".bar");
-        const mobileNavEl = document.querySelector("div.links");
-        let isMobNav = false;
-        if (window.innerWidth <= 1000) {
-                desktopEls.forEach(el => {
-                    el.style.display = "none";
-                });
-                mobileEls.forEach(el => {
-                    el.style.display = "block";
-                })
-        }
-        window.addEventListener('resize', () => {
-            if (window.innerWidth <= 1000) {
-                desktopEls.forEach(el => {
-                    el.style.display = "none";
-                });
-                mobileEls.forEach(el => {
-                    el.style.display = "block";
-                })
-            } else {
-               desktopEls.forEach(el => {
-                    el.style.display = "block";
-                });
-                mobileEls.forEach(el => {
-                    el.style.display = "none";
-                }) 
-            }
-        })
-        barEl.addEventListener("click", (e) => {
-            e.stopImmediatePropagation()
-            mobileNavEl.classList.toggle('show');
-        })
-    }, [])
-    return (
-        <>
-            <nav className="navbar">
-                <ul className="left-side">
-                    <li className="imgLi"><img src={Logo} alt="The color-inverted form of the Inscribe Logo" /> <span>Inscribe</span></li>
-                    <li className="onDesktop">
-                        <a href="#home">Home</a>
-                    </li>
-                    <li className="onDesktop">
-                        <a href="#plans">Plans</a>
-                    </li>
-                    <li className="onDesktop">
-                        <a href="#features">Features</a>
-                    </li>
-                </ul>
-                <ul className="right-side">
-                    <li className="onDesktop">
-                        <a href="#help">Help</a>
-                    </li>
-                    <li className="onDesktop">
-                        <Button className="sc-btn inv-sc-btn" value="Get Started" url={props.url}/>
-                    </li>
-                    <img src={bars} alt="Three black horizontal bars" className="onMobile bar"/>
-                </ul>
-            </nav>
-            <div className="links">
-                <ul>
-                    <li>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#plans">Plans</a>
-                    </li>
-                    <li>
-                        <a href="#help">Help</a>
-                    </li>
-                    <li>
-                        <a href="#">Get Started</a>
-                    </li>
-                </ul>
-            </div>
-        </>
-    )
+  useEffect(() => {
+    const desktopEls = document.querySelectorAll(".onDesktop");
+    const mobileEls = document.querySelectorAll(".onMobile");
+    const barEl = document.querySelector(".bar");
+    const mobileNavEl = document.querySelector("div.links");
+    let isMobNav = false;
+    if (window.innerWidth <= 1000) {
+      desktopEls.forEach((el) => {
+        el.style.display = "none";
+      });
+      mobileEls.forEach((el) => {
+        el.style.display = "block";
+      });
+    }
+    window.addEventListener("resize", () => {
+      if (window.innerWidth <= 1000) {
+        desktopEls.forEach((el) => {
+          el.style.display = "none";
+        });
+        mobileEls.forEach((el) => {
+          el.style.display = "block";
+        });
+      } else {
+        desktopEls.forEach((el) => {
+          el.style.display = "block";
+        });
+        mobileEls.forEach((el) => {
+          el.style.display = "none";
+        });
+      }
+    });
+    barEl.addEventListener("click", (e) => {
+      e.stopImmediatePropagation();
+      mobileNavEl.classList.toggle("show");
+    });
+  }, []);
+  return (
+    <>
+      <nav className="navbar">
+        <ul className="left-side">
+          <li className="imgLi">
+            <img
+              src={Logo}
+              alt="The color-inverted form of the Inscribe Logo"
+            />{" "}
+            <span>Inscribe</span>
+          </li>
+          <li className="onDesktop">
+            <a href="#home">Home</a>
+          </li>
+          <li className="onDesktop">
+            <a href="#plans">Plans</a>
+          </li>
+          <li className="onDesktop">
+            <a href="#features">Features</a>
+          </li>
+        </ul>
+        <ul className="right-side">
+          <li className="onDesktop">
+            <a href="#help">Help</a>
+          </li>
+          <li className="onDesktop">
+            <Button
+              className="sc-btn inv-sc-btn"
+              value="Get Started"
+              url={props.url}
+            />
+          </li>
+          <img
+            src={bars}
+            alt="Three black horizontal bars"
+            className="onMobile bar"
+          />
+        </ul>
+      </nav>
+      <div className="links">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#plans">Plans</a>
+          </li>
+          <li>
+            <a href="#help">Help</a>
+          </li>
+          <li>
+            <a href={props.url}>Get Started</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
--- react-app/src/pages/home.jsx
+++ react-app/src/pages/home.jsx
@@ -41,17 +41,20 @@ export default function Home() {
   return (
     <>
       <Navbar url={browserLink} />
-      <section>
+      <section className="mb">
         <article className="introSection" id="home">
           <article className="content">
-            <h1 data-aos="fade-down">The browser extension for anyone</h1>
+            <h1 data-aos="fade-down" className="main-txt">
+              The browser extension for anyone
+            </h1>
             <p data-aos="fade-up">
               Unleash your creativity with Inscribe, the note-taking extension.{" "}
-              <br /> Take your productivity to the next level.
+              <br />
+              <span>Take your productivity to the next level.</span>
             </p>
             <article data-aos="fade-left">
               <Button
-                className="sc-btn nrm-sc-btn"
+                className="sc-btn inv-sc-btn"
                 value="Get Started"
                 url={browserLink}
               />
@@ -65,53 +68,53 @@ export default function Home() {
             </article>
           </article>
         </article>
-      </section>
-      <article className="features" id="features">
-        <h2>Features</h2>
-        <article className="featuresContainer">
-          <article
-            className="feature"
-            data-aos={window.innerWidth <= 1000 ? "fade-right" : "fade-up"}
-          >
-            <img src={stickyNote} alt="A sticky note icon" />
-            <p className="feahead">Sticky Notes</p>
-            <p>
-              Effortless capture thoughts on this versatile digital sticky note-
-              pops up on your laptop, customizable colours and conveniently
-              rests on either side.{" "}
-            </p>
-          </article>
-          <article
-            className="feature"
-            data-aos={window.innerWidth <= 1000 ? "fade-left" : "fade-up"}
-          >
-            <img src={pencil} alt="A pencil icon" />
-            <p className="feahead">Drawing features</p>
-            <p>
-              Our drawing feature allows you to scribble and sketch your ideas
-              while browsing. Customize your strokes with adjustable fonts and
-              fun colours that make working fun!
-            </p>
-          </article>
-          <article
-            className="feature"
-            data-aos={window.innerWidth <= 1000 ? "fade-right" : "fade-up"}
-          >
-            <img src={download} alt="A download icon" />
-            <p
-              className="feahead"
-              role="heading"
-              aria-roledescription="heading"
+        <article className="features" id="features">
+          <h2>Features</h2>
+          <article className="featuresContainer">
+            <article
+              className="feature"
+              data-aos={window.innerWidth <= 1000 ? "fade-right" : "fade-up"}
             >
-              Save notes locally
-            </p>
-            <p>
-              This feature offers excellent accessibility by enabling you to
-              keep your notes and other vital data locally on your computer.
-            </p>
+              <img src={stickyNote} alt="A sticky note icon" />
+              <p className="feahead">Sticky Notes</p>
+              <p>
+                Effortless capture thoughts on this versatile digital sticky
+                note- pops up on your laptop, customizable colours and
+                conveniently rests on either side.{" "}
+              </p>
+            </article>
+            <article
+              className="feature"
+              data-aos={window.innerWidth <= 1000 ? "fade-left" : "fade-up"}
+            >
+              <img src={pencil} alt="A pencil icon" />
+              <p className="feahead">Drawing features</p>
+              <p>
+                Our drawing feature allows you to scribble and sketch your ideas
+                while browsing. Customize your strokes with adjustable fonts and
+                fun colours that make working fun!
+              </p>
+            </article>
+            <article
+              className="feature"
+              data-aos={window.innerWidth <= 1000 ? "fade-right" : "fade-up"}
+            >
+              <img src={download} alt="A download icon" />
+              <p
+                className="feahead"
+                role="heading"
+                aria-roledescription="heading"
+              >
+                Save notes locally
+              </p>
+              <p>
+                This feature offers excellent accessibility by enabling you to
+                keep your notes and other vital data locally on your computer.
+              </p>
+            </article>
           </article>
         </article>
-      </article>
+      </section>
       <article className="plans" data-aos="fade-up" id="plans">
         <h2>Inscribe Plans</h2>
         <p>Choose a plan that best suits your needs! </p>
@@ -202,96 +205,103 @@ export default function Home() {
           </p>
         </article>
       </article>
-      <article className="howItWorks" id="how">
-        <img
-          src={completed}
-          alt="An illustraions of a person chechong boxes"
-          data-aos="fade-right"
-        />
-        <article className="content" data-aos="fade-left">
-          <h2>How it works</h2>
-          <ul>
-            <li>
-              Open your browser (Microsoft Edge or Chrome) and search for the
-              Inscribe extension{" "}
-            </li>
-            <li>Download the extension and add to your extension shortcut.</li>
-            <li> To use it, click on the extension icon and enjoy!</li>
-            <li>
-              Also add sticky notes to any website, rightclick and click the
-              "Add an Inscribe Sticky Note here" option.
-            </li>
-            <li>
-              The sticky note now pops up at the upper right side of your
-              computer (it can move around to your desired position).
-            </li>
-            <li>
-              The sticky note can also be expanded to accommodate your needs, it
-              is done by clicking the clicking and holding on the bottom right
-              corner.
-            </li>
-          </ul>
-          <p>
-            You can now browse and take note effortlessly and save your document
-            locally. To activate the drawing or scribble feature, you can
-            subscribe to our premium plan which is only $2.99 a month!
-            <br />
-            <br />
-            For more information{" "}
-            <a href="https://www.instagram.com/inscribe.ext/" target="_blank">
-              Check out our Instagram page
-            </a>
-          </p>
-        </article>
-      </article>
-      <article className="helpSection" data-aos="fade-up" id="help">
-        <article className="socialLinks">
-          <img src={Logo} alt="Inscribe Logo" />
-          <h2>
-            Unleash your creativity with Inscribe. Take your productivity to the
-            next level.
-          </h2>
-          <article className="row">
-            <a
-              href="https://www.facebook.com/profile.php?id=61556646921708"
-              target="_blank"
-            >
-              <img src={facebook} alt="The gray version of the facebook icon" />
-            </a>
-            <a href="https://twitter.com/inscribeExt" target="_blank">
-              <img
-                src={xTwitter}
-                alt="The gray version of the X(Twitter) icon"
-              />
-            </a>
-            <a href="https://www.instagram.com/inscribe.ext/" target="_blank">
-              <img
-                src={instagram}
-                alt="The gray version of the instagram icon"
-              />
-            </a>
+      <section className="mb">
+        <article className="howItWorks" id="how">
+          <img
+            src={completed}
+            alt="An illustraions of a person chechong boxes"
+            data-aos="fade-right"
+          />
+          <article className="content" data-aos="fade-left">
+            <h2>How it works</h2>
+            <ul>
+              <li>
+                Open your browser (Microsoft Edge or Chrome) and search for the
+                Inscribe extension{" "}
+              </li>
+              <li>
+                Download the extension and add to your extension shortcut.
+              </li>
+              <li> To use it, click on the extension icon and enjoy!</li>
+              <li>
+                Also add sticky notes to any website, rightclick and click the
+                "Add an Inscribe Sticky Note here" option.
+              </li>
+              <li>
+                The sticky note now pops up at the upper right side of your
+                computer (it can move around to your desired position).
+              </li>
+              <li>
+                The sticky note can also be expanded to accommodate your needs,
+                it is done by clicking the clicking and holding on the bottom
+                right corner.
+              </li>
+            </ul>
+            <p>
+              You can now browse and take note effortlessly and save your
+              document locally. To activate the drawing or scribble feature, you
+              can subscribe to our premium plan which is only $2.99 a month!
+              <br />
+              <br />
+              For more information{" "}
+              <a href="https://www.instagram.com/inscribe.ext/" target="_blank">
+                Check out our Instagram page
+              </a>
+            </p>
           </article>
         </article>
-        <article className="webLinks">
-          <h3>COMPANY</h3>
-          <ul>
-            <li>
-              <a href="#how">How it works</a>
-            </li>
-            <li>
-              <a href="#plans">Plans</a>
-            </li>
-          </ul>
+        <article className="helpSection" data-aos="fade-up" id="help">
+          <article className="socialLinks">
+            <img src={Logo} alt="Inscribe Logo" />
+            <h2>
+              Unleash your creativity with Inscribe. Take your productivity to
+              the next level.
+            </h2>
+            <article className="row">
+              <a
+                href="https://www.facebook.com/profile.php?id=61556646921708"
+                target="_blank"
+              >
+                <img
+                  src={facebook}
+                  alt="The gray version of the facebook icon"
+                />
+              </a>
+              <a href="https://twitter.com/inscribeExt" target="_blank">
+                <img
+                  src={xTwitter}
+                  alt="The gray version of the X(Twitter) icon"
+                />
+              </a>
+              <a href="https://www.instagram.com/inscribe.ext/" target="_blank">
+                <img
+                  src={instagram}
+                  alt="The gray version of the instagram icon"
+                />
+              </a>
+            </article>
+          </article>
+          <article className="webLinks">
+            <h3>COMPANY</h3>
+            <ul>
+              <li>
+                <a href="#how">How it works</a>
+              </li>
+              <li>
+                <a href="#plans">Plans</a>
+              </li>
+            </ul>
+          </article>
+          <article className="contactUs">
+            <h3>Contact Us</h3>
+            <p>
+              For further inquiries reach out to us on our socials or email us
+              at inscribeext@gmail{" "}
+            </p>
+          </article>
         </article>
-        <article className="contactUs">
-          <h3>Contact Us</h3>
-          <p>
-            For further inquiries reach out to us on our socials or email us at
-            inscribeext@gmail{" "}
-          </p>
-        </article>
-      </article>
-      <footer>
+      </section>
+      <footer className="ft">
         <p>&copy; 2024 Incribe inc.</p>
         <div className="left">
           <a href="https://shorturl.at/kpwx7" target="_blank">
